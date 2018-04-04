65e44323c82fd96c9c4ebf2d54d1748570562142
Prashant jha <prashant7196@gmail.com>
2018-04-04 14:08:36 +0530

    introduction

---
+++ introduction/src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+	public static void main(String args[]){
+		//System.out.print("hello world");
+		byte var1 =-127; //creating a variable of size byte
+		int var2 = 127;  //creating a variable of size int
+		
+		//byte A= var1+var2; // this line will cause error of "Type mismatch".
+		
+		int A = var1+var2;
+		System.out.println(A);
+		
+	}
+
+}
